9cb651d47c8ef671d81dfd754b543eeb981957e6
P.Jaswanth Krishna <jaswanthuchiha69@gmail.com>
2025-11-17 17:11:32 +0530

    added 'Achievements' link to navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ const navLinks = [
   { name: 'About', href: '#about' },
   { name: 'Skills', href: '#skills' },
   { name: 'Projects', href: '#projects' },
+  { name: 'Achievements', href: '#achievements' },
   { name: 'Contact', href: '#contact' },
 ];
 
